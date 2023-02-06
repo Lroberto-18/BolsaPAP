4881718c835480c97d23b09bf249bb8b5bd7d8f2
lroberto-18 <leandroroberto582@gmail.com>
2023-02-06 11:55:15 -0300

    Revert "updates"
    This reverts commit e4eac0726379918104d680eff7ff05a9f7a09e1c.

--- RF_2022.py
+++ RF_2022.py
@@ -1,105 +1,49 @@
 import pandas as pd
 import os
-#extrair dados dos relatórios finais 2022 enviado via Google Forms
+#extrair dados dos relatórios finais 2022 enviado via Forms
 
+words = ['DOCENTES','Nome','TÉCNICOS ADMINISTRATIVOS (servidores e/ou terceirizados)','ALUNOS BOLSISTAS']
+way = os.listdir('PLN') #Diretório contendo todas as planihas
 
-def teachers(keywords, path_team):
-  name = []
-  siape = []
+#Extrair Nome e Siape Docentes
+name = []
+siape = []
+for i in range(len(way)):
+  df = pd.read_excel('PLN/'+way[i], sheet_name='equipe_extensionista')
+  for i in range(2,df.shape[0]):
+    if(df.at[i,'Unnamed: 0']!=words[2] and str(df.at[i,'Unnamed: 0'])!='nan'):
+      name.append(str(df.at[i,'Unnamed: 0']))
+      siape.append(df.at[i,'Unnamed: 4'])
+    if(df.at[i,'Unnamed: 0']==words[2]):
+      break
+print(len(siape)==len(name))
+#salvando em csv
+data = {'nome':name,'siape':siape}
+data_frame = pd.DataFrame(data)
+data_frame.to_csv('docentes.csv')
 
-  for i in range(len(path_team)):
-    df = pd.read_excel('equipe/'+path_team[i], sheet_name='equipe_extensionista')
-
-    for i in range(2,df.shape[0]):
-      if(df.at[i,'Unnamed: 0']!=keywords[2] and str(df.at[i,'Unnamed: 0'])!='nan'):
-        name.append(str(df.at[i,'Unnamed: 0']).upper())
-        siape.append(df.at[i,'Unnamed: 4'])
-      if(df.at[i,'Unnamed: 0']==keywords[2]):
-        break
-
-  data = {'nome':name,'siape':siape}
-  data_frame = pd.DataFrame(data)
-  data_frame.to_csv('docentes.csv')
-
-
-def technical(keywords, path_team):
-  name = []
-  siape = []
-  func = []
-
-  for i in range(len(path_team)):
-    dframe = pd.read_excel('equipe/'+path_team[i], sheet_name='equipe_extensionista')
-    control=True
-    j,ref0,ref1=0,0,0
-
-    while(control):
-        if(dframe.at[j,'Unnamed: 0']==keywords[2]):
-          ref0=j+2
-        if(dframe.at[j,'Unnamed: 0']==keywords[3]):
-          ref1=j
-          control=False
-        j+=1
-
-    for z in range(ref0,ref1):
-      if(dframe.at[z,'Unnamed: 0']!=keywords[2] and str(dframe.at[z,'Unnamed: 0'])!='nan'):
-          name.append(str(dframe.at[z,'Unnamed: 0']).upper())
-          siape.append(dframe.at[z,'Unnamed: 1'])
-          func.append(dframe.at[z,'Unnamed: 2'])
-
-  
-  data = {'nome':name,'siape':siape,'função':func}
-  data_frame = pd.DataFrame(data)
-  data_frame.to_csv('tecnicos.csv')
-
-
-def social_media(path_social_media):
-  link = []
-  qtd = []
-
-  for i in range(len(path_social_media)):
-    df = pd.read_excel('redes/'+path_social_media[i], sheet_name='Planilha1')
-
-    for i in range(2,df.shape[0]):
-      link.append(str(df.at[i,'Unnamed: 0']))
-      qtd.append(df.at[i,'Unnamed: 1'])
-
-  data = {'link':link,'qtd':qtd}
-  data_frame = pd.DataFrame(data)
-  data_frame.to_csv('redes.csv')
-
-
-def locations(path_locations):
-  atv = []
-  nome = []
-  uf = []
-  cidade = []
-  bairro = []
-
-  for i in range(len(path_locations)):
-    df = pd.read_excel('locais/'+path_locations[i], sheet_name='locais')
-    for i in range(2,df.shape[0]):
-      atv.append(str(df.at[i,'Unnamed: 0']))
-      nome.append(str(df.at[i,'Unnamed: 1']))
-      uf.append(str(df.at[i,'Unnamed: 2']))
-      cidade.append(str(df.at[i,'Unnamed: 3']))
-      bairro.append(str(df.at[i,'Unnamed: 4']))
-
-  data = {'atv':atv,'nome':nome,'uf':uf,'cidade':cidade,'bairro':bairro}
-  data_frame = pd.DataFrame(data)
-  data_frame.to_csv('locais.csv')
-
-
-def main():
-  keywords = ['DOCENTES','Nome','TÉCNICOS ADMINISTRATIVOS (servidores e/ou terceirizados)','ALUNOS BOLSISTAS']
-  path_team = os.listdir('equipe')
-  path_social_media = os.listdir('redes')
-  path_locations = os.listdir('locais')
-
-  teachers(keywords, path_team)
-  technical(keywords, path_team)
-  social_media(path_social_media)
-  locations(path_locations)
-
-
-if __name__=="__main__":
-  main()
+#Extrair Nome e Siape Técnicos Adminstrativos
+name = []
+siape = []
+func = []
+for i in range(len(way)):
+  dframe = pd.read_excel('PLN/'+way[i], sheet_name='equipe_extensionista')
+  control=True
+  j,ref0,ref1=0,0,0
+  while(control):
+      if(dframe.at[j,'Unnamed: 0']==words[2]):
+        ref0=j+2
+      if(dframe.at[j,'Unnamed: 0']==words[3]):
+        ref1=j
+        control=False
+      j+=1
+  for z in range(ref0,ref1):
+    if(dframe.at[z,'Unnamed: 0']!=words[2] and str(dframe.at[z,'Unnamed: 0'])!='nan'):
+        name.append(str(dframe.at[z,'Unnamed: 0']))
+        siape.append(dframe.at[z,'Unnamed: 1'])
+        func.append(dframe.at[z,'Unnamed: 2'])
+print(len(siape)==len(name))
+#salvando em csv
+data = {'nome':name,'siape':siape,'função':func}
+data_frame = pd.DataFrame(data)
+data_frame.to_csv('tecnicos.csv')
